a5a626efa9020ead8ace16b0cddd3773c6096bd0
Daniel Lopes <viana.daniel02@gmail.com>
2018-11-10 22:45:37 -0300

    Implementado pesquisa de GIFs

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -47,6 +47,11 @@ class _HomePageState extends State<HomePage> {
               ),
               style: TextStyle(color: Colors.white, fontSize: 18.0),
               textAlign: TextAlign.center,
+              onSubmitted: (text){
+                setState(() {
+                  _search = text;
+                });
+              },
             ),
           ),
           Expanded(
